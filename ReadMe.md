e5290bc0fde932570741702b3151bbc9dd083c74
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-20 18:41:47 +0530

    new activity
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Activity life cycle
+https://developer.android.com/guide/components/activities/activity-lifecycle.html
--- app/src/main/java/com/codingblocks/activity/MainActivity.java
+++ app/src/main/java/com/codingblocks/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codingblocks.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,21 +9,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResult;
     EditText etNum1;
     EditText etNum2;
     Button btnAdd;
+    Button btnopen;
     public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
         btnAdd = (Button) findViewById(R.id.btnAdd);
         tvResult = (TextView) findViewById(R.id.tvResult);
         etNum1 = (EditText) findViewById(R.id.etNum1);
         etNum2 = (EditText) findViewById(R.id.etNum2);
+        btnopen = (Button) findViewById(R.id.btnOpen);
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +38,47 @@ public class MainActivity extends AppCompatActivity {
                 long num2 = Long.parseLong(etNum2.getText().toString());
                 Log.d(TAG, "onClick: ");
                 tvResult.setText(String.valueOf(num1 + num2));
+
             }
         });
+
+        btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SendActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
     }
 }
