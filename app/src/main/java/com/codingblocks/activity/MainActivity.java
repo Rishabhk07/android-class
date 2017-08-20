49eee78dbb6e07a4b4b637df2fc87301b241574a
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-20 17:13:45 +0530

    addition
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/codingblocks/activity/MainActivity.java
+++ app/src/main/java/com/codingblocks/activity/MainActivity.java
@@ -2,13 +2,36 @@ package com.codingblocks.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvResult;
+    EditText etNum1;
+    EditText etNum2;
+    Button btnAdd;
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(etNum1.getText().toString());
+                long num2 = Long.parseLong(etNum2.getText().toString());
+                Log.d(TAG, "onClick: ");
+                tvResult.setText(String.valueOf(num1 + num2));
+            }
+        });
     }
 }
